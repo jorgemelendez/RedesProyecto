4d1ea548d59b3f59ee4d4c3cd0561c63e0be1c03
chrisb6610 <chrisb66110@gmail.com>
2018-09-29 23:02:09 -0600

    Odenar despues de fase 1

--- RedesChritofer/parametros.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-if __name__ == '__main__':
-	print ("Número de parámetros: ", len(sys.argv) )
-	print ("Lista de argumentos: ", sys.argv )
-	if len(sys.argv) == 4:
-		print (sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3])
