28479cfe6c13ffc5edca25f206dcc9fea43a8a85
chrisb6610 <chrisb66110@gmail.com>
2018-10-20 03:02:13 -0600

    Se creo codigo para generar archivos

--- FaseII/HiloConexionUDPSegura.py
+++ FaseII/HiloConexionUDPSegura.py
@@ -335,4 +335,5 @@ if __name__ == '__main__':
 	threadEmisor = threading.Thread(target=server.cicloServer, args=())
 	threadEmisor.start()
 	emisor.enviarArchivo()
+	time.sleep(10)
 	bitacora.terminar()
