6cef62f2be35cb3e5f6980f08fe07587c84dd171
Jorge Melendez <jorge.melendez.s98@gmail.com>
2018-11-07 18:35:30 -0600

    Clase que divide la topologia y la pone en diccionario y ejemplo

--- FaseIII/CSVTopologia.py
+++ FaseIII/CSVTopologia.py
@@ -2,22 +2,38 @@ import csv
 
 
 class CSVTopologia:
-	nombrearchivo
-	ip
-	mascara
-	puerto
-	ip
-	mascara
+	self.listaTuplas = dic()
 
 	def __init__(self, nombrearchivo):
 		self.nombrearchivo = nombrearchivo
+		llenaDiccionario(self.nombrearchivo);
 
-	def getIpMio(self):
+	def llenaDiccionario(self):
+		for line in leearchivo:
+			listaTupla = line.split(',')
 
-	def getIpOtro(self):
+		idConexion = listaTuplas[0], listaTuplas[1], listaTuplas[2]
+		hayKey = self.listaTuplas.get(idConexion)
+		otroIpDistancia = listaTuplas[3],listaTuplas[4],listaTuplas[5], listaTuplas[6]
 
-	def getMascarMio(self):
+		if hayKey is None:
+			listaDireccion = list()
+			listaDireccion.append()
+			self.listaTuplas[idConexion] = listaDireccion
+		else
+			listaDireccion.append
 
-	def getPuertoMio(self):
+		if listaMensajes is None:
+			listaMensajes = list()
+			listaMensajes.append(otroIpDistancia)
+			self.buzon[idConexion] = listaMensajes
+		else:
+			listaMensajes.append(otroIpDistancia)
+
+
+	def getDiccionario(self):
+		return listaTuplas
+
+
 
 
